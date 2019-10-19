aa4abed2d3cb5711afa29838396859b2aab6dc7e
gup-shivam <sgupta.letswork@gmail.com>
2019-10-19 21:18:41 +0530

    dfgff

---
+++ chat_app/lib/widgets/ChatAppBar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:chat_app/config/Assets.dart';
+import 'package:chat_app/config/Pallete.dart';
+import 'package:flutter/semantics.dart';
+
+class ChatAppBar extends StatelessWidget {
+  final double height = 100;
+  @override
+  Widget build(BuildContext context) {}
+  @override 
+  Size get prefferedSize => Size.fromHeight(height);
+}
